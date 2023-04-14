cf2d6996b98b6f41f51c69d4613df58c25af710b
OSVALDO GARCES C <osvaldogarcescardenas@gmail.com>
2023-04-14 17:45:32 -0500

    Actualizacion Medico y pacienteController

--- HospitalServices/Controllers/PacienteController.cs
+++ HospitalServices/Controllers/PacienteController.cs
@@ -22,6 +22,12 @@ namespace HospitalServices.Controllers
             //return dbHospital.Pacientes.Select(x => new PacienteInfo { Nombre = x.Nombre, Apellido = x.Apellido, Teléfono = x.Teléfono, Dirección = x.Dirección }).ToList();
             return dbHospital.Pacientes.ToList();
         }
+       
+        //public List<Paciente> GETACTIVE()
+        //{
+        //     //return dbHospital.Pacientes.Where(x => x.Ingresoes.Where(z => z.Fecha_salida == null).FirstOrDefault().Fecha_salida == null).ToList();
+        //    return (from p in dbHospital.Pacientes join i in dbHospital.Ingresoes on p.ID equals i.ID_Paciente where i.Fecha_salida == null && i.Fecha_ingreso != null select p).ToList();
+        //}
 
         // GET: Paciente/Details/5
         //[HttpGet]
