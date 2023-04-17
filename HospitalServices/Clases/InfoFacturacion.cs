cf4cedc93c5bbf440d77b8cbbd91819df02601d8
John Andrei <johnandreiflorez@gmail.com>
2023-04-16 21:25:36 -0500

    Facturacion

---
+++ HospitalServices/Clases/InfoFacturacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalServices.Clases
+{
+    public class InfoFacturacion
+    {
+        public int IDIngreso { get; set; }
+        public string Paciente { get; set; }
+        public string Cedula{ get; set; }
+        public string FechaIngreso { get; set; }
+        public string Habitacion { get; set; }
+        public Decimal PrecioHabitacion { get; set; }
+
+    }
+}
