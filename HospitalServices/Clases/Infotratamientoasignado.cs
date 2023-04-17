fb36b0dae5104388d8cfa2189e2b760b9a1d7304
jorge98707070 <jivacorre@gmail.com>
2023-04-16 21:45:43 -0500

    Se actualiza tratamiento asignado

---
+++ HospitalServices/Clases/Infotratamientoasignado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalServices.Clases
+{
+    public class Infotratamientoasignado
+    {
+        public int ID_Tratamiento { get; set; }
+        public int ID_Ingreso { get; set; }
+        public string Fecha_Inicio { get; set; }
+        public string Fecha_Fin { get; set; }
+    }
+}
