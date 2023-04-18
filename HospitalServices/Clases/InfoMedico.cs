e68085ba106c639780e299508df068d7a3fb0964
OSVALDO GARCES C <osvaldogarcescardenas@gmail.com>
2023-04-17 19:54:01 -0500

    Uprdate de Atencion y Medico

--- HospitalServices/Clases/InfoMedico.cs
+++ HospitalServices/Clases/InfoMedico.cs
@@ -5,13 +5,15 @@ using System.Web;
 
 namespace HospitalServices.Clases
 {
-    public class Medico
+    public class InfoMedico
     {
+        public int ID { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Especialidad { get; set; }
         public string Direccion { get; set; }
         public string Telefono { get; set; }
+        public int ID_Especializacion { get; set; }
 
     }
 }
